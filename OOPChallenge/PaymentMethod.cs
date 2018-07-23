35e8009784d6a283452ff7be530114e893290b54
Michael Sogamoso <mssg_2211@hotmail.com>
2018-07-23 10:11:20 -0500

    PaymentMethod class added

---
+++ OOPChallenge/PaymentMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPChallenge
+{
+    class PaymentMethod
+    {
+        private int id;
+        public int Id { get => id; set => id = value; }
+        public void authorize()
+        {
+
+        }
+    }
+}
