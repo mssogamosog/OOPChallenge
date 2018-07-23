b4b07dda3cd69d550e83c39cdd38a8ade90c9883
Michael Sogamoso <mssg_2211@hotmail.com>
2018-07-23 13:43:12 -0500

    Order class implemented

--- OOPChallenge/LineItem.cs
+++ OOPChallenge/LineItem.cs
@@ -11,5 +11,9 @@ namespace OOPChallenge
         private int quantity;
         private double price;
         private Product product;
+
+        public int Quantity { get => quantity; set => quantity = value; }
+        public double Price { get => price; set => price = value; }
+        internal Product Product { get => product; set => product = value; }
     }
 }
